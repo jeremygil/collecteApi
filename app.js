4ea762cbf8c5874ecd7999ef51fcf6567be8e76c
jeremygil <jeremy.gilger@hotmail.fr>
2019-12-20 17:09:35 +0100

    fix

--- app.js
+++ app.js
@@ -1,22 +1,7 @@
 var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser');
+	app = express();
 
-// Nous définissons ici les paramètres du serveur.
-var hostname = 'localhost';
-var port = process.env.port || 3000;
+var listRoutes = require('./routes/listRoutes'); //importing route
+app.use('/v1', listRoutes);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-var routes = require('./routes/listRoutes'); //importing route
-routes(app); //register the route
-
-
-//https://jsonplaceholder.typicode.com/todos/
-
-
-// Démarrer le serveur 
-app.listen(port, hostname, function () {
-	console.log("Mon serveur fonctionne sur http://" + hostname + ":" + port + "\n");
-});
+module.exports = app;
---
+++ routes/listRoutes.js
@@ -0,0 +1,6 @@
+var express = require('express'),
+    router = express.Router(),
+    bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(bodyParser.json());
